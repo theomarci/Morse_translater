c3b2d40650379ab7f387a2c746130dae8a284938
Théo <theos9mar@gmail.com>
2024-08-07 10:26:03 +0200

    rename functions and comment my code in main.py

--- translater/main.py
+++ translater/main.py
@@ -1,7 +1,7 @@
 import module_morse
 import module_fr_into_morse
 
-def Traductor(morse_text):
+def Morse_translation(morse_text):
     split_text = morse_text.split(" ")
     trad_morse = []
 
@@ -10,10 +10,10 @@ def Traductor(morse_text):
         trad_morse.append(morse)
     
     string_trad = ''.join(trad_morse)
-    print(string_trad)
-    return
+    
+    return print(string_trad)
 
-def Fr_traduction (fr_text):
+def Fr_translation (fr_text):
     letter_list = []
     text = []
 
@@ -25,25 +25,24 @@ def Fr_traduction (fr_text):
         text.append(in_morse)
 
     string_in_morse = ' '.join(text)
-    print(string_in_morse)
 
-    return
+    return print(string_in_morse)
 
 morse_text = ".--- .----. .- .."
 
-Traductor(morse_text)
+Morse_translation(morse_text)
 
 morse_text = "-.-. --- -. -. .- .-. -.. -.-.-----. / - ..- / . ... - / -.-. .... .. .- -. - -.-.-----."
-Traductor( morse_text)
+Morse_translation( morse_text)
 
 morse_text = """...- . ..- -..- / - ..- / . - .-. . / -- .- / - . -- --- .. -. ..--.."""
-Traductor(morse_text)
+Morse_translation(morse_text)
 
 morse_text = """-.-. --- -- -- . - / ...- .- ... / - ..- ..--.."""
-Traductor(morse_text)
+Morse_translation(morse_text)
 
 morse_text = """.. / .-.. --- ...- . / -.-- --- ..- / ... --- / -- ..- -.-. .... -.-.-----."""
-Traductor(morse_text)
+Morse_translation(morse_text)
 
 morse_text = ("--- .... -.-.-----. / .-.. .- / .-.. .- -.-.-----. /"
               " -- .- .. ... / .--. --- ..- .-. .--. ..- --- .. / "
@@ -56,13 +55,13 @@ morse_text = ("--- .... -.-.-----. / .-.. .- / .-.. .- -.-.-----. /"
               "--.- ..- .. / ...- . ..- .-.. . -. - / -. --- ..- ... / - ..- . .-. / ..--.. / "
               ". - / -.-. --- -- -- . -. - / --- -. / ...- .- / ..-. .- .. .-. . / "
               ".--. --- ..- .-. / -. . - - --- -.-- . .-. / -. --- ... / ...- .-..- - . -- . -. - ... / ..--..")
-Traductor(morse_text)
+Morse_translation(morse_text)
 
 fr_text = "j'ai eu un coup de foudre!"
-Fr_traduction(fr_text)
+Fr_translation(fr_text)
 
 fr_text = "you're so kind!"
-Fr_traduction(fr_text)
+Fr_translation(fr_text)
 
 fr_text = ("elle se prend pas pour une bouse de troll, celle-là. qu'est-ce qu'on ferait sans elle ? "
            "pour commencer, on arrèterait d'avoir tout le temps des conversations incompréhensibles. "
@@ -70,4 +69,4 @@ fr_text = ("elle se prend pas pour une bouse de troll, celle-là. qu'est-ce qu'o
            "sur l'orientation pipeautesque de l'énergie de mon cul. si elle n'était pas là, et bien... on aurait pas été téléportés par erreur dans le chateau de gzor, "
            "et je n'aurais pas perdu mon unique point de destin dans une bataille contre la taupe-garous géante! et on serait sortis normalement de la forèt de schlipak, "
            "et on aurait fait l'aventure quand mème, parce que sa magie, elle n'a servi à rien.")
-Fr_traduction(fr_text)
+Fr_translation(fr_text)
